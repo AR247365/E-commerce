30237c85d2bce14bb5a19f3906fa9fa2ec387260
AR247365 <lonewolf22081998@gmail.com>
2023-09-28 18:22:47 +0530

    Filter

--- store/src/components/Filters.jsx
+++ store/src/components/Filters.jsx
@@ -1,7 +1,20 @@
 import React from "react"
+import { Form, useLoaderData, Link } from "react-router-dom"
+import FormInput from "./FormInput"
 
 const Filters = () => {
-  return <h1>Filters</h1>
+  return (
+    <Form>
+      <FormInput
+        type="search"
+        name="search"
+        label="search product"
+        size="input-sm"
+      />
+      <button>Search</button>
+      <Link to="/products">clear</Link>
+    </Form>
+  )
 }
 
 export default Filters
