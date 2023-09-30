bf15176659ecd3603ff01982386c9c6032545b81
AR247365 <lonewolf22081998@gmail.com>
2023-09-30 18:00:12 +0530

    Filter, Pagination

--- store/src/components/PaginationContainer.jsx
+++ store/src/components/PaginationContainer.jsx
@@ -1,6 +1,17 @@
 import React from "react"
+import { useLoaderData, useLocation, useNavigate } from "react-router-dom"
 
 const PaginationContainer = () => {
+  const { meta } = useLoaderData()
+  const { pageCount, page } = meta.pagination
+  const pages = Array.from({ length: pageCount }, (_, index) => {
+    return index + 1
+  })
+
+  const handlePageChange = (pageNumber) => {
+    console.log(pageNumber)
+  }
+  if (pageCount < 2) return null
   return <h1>PaginationContainer</h1>
 }
 
