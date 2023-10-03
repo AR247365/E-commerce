8ef06f60ad16bc38614ec7571d69f850c782d8f0
AR247365 <lonewolf22081998@gmail.com>
2023-10-03 18:19:32 +0530

    Cart

--- store/src/components/CartItemsList.jsx
+++ store/src/components/CartItemsList.jsx
@@ -1,11 +1,16 @@
 import React from "react"
+import { useSelector } from "react-redux"
+import CartItem from "./CartItem"
 
 const CartItemsList = () => {
+  const cartItems = useSelector((state) => state.cartState.cartItems)
+
   return (
-    <div>
-      <h1>CartItemsList</h1>
-    </div>
+    <>
+      {cartItems.map((item) => {
+        return <CartItem key={item.cartID} cartItem={item} />
+      })}
+    </>
   )
 }
-
 export default CartItemsList
